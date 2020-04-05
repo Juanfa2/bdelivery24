45f3bce46c90396a587a52f7c64f6f548b723352
Luis Franceschi <luiggi_ff@yahoo.com.ar>
2020-04-05 10:10:17 -0300

    agregado service mysql stop y chequeos y limpieza antes de levantar un nuevo docker en mysql

--- start-db.sh
+++ start-db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo service mysql stop
+
 docker -v
 if [ $? -ne 0 ]; then
    sudo apt update
@@ -8,10 +10,24 @@ if [ $? -ne 0 ]; then
    newgrp docker
 fi
 
-docker run --name mysql                     \
+
+if [ ! "$(docker ps -q -f name=mysql)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f status=created -f name=mysql)" ]; then
+        # cleanup
+        echo "El contenedor mysql está detenido, eliminando"
+        docker rm mysql
+    fi
+    
+    echo "Iniciando nuevo contenedor mysql"
+    docker run --name mysql                 \
             -p 3306:3306                    \
             -e MYSQL_ROOT_PASSWORD=Rahziec2 \
             -e MYSQL_DATABASE=bd2_grupo24   \
             -e MYSQL_USER=grupo24           \
             -e MYSQL_PASSWORD=eiquib5A      \
             -d mysql:5.7
+
+else 
+    echo "El contenedor mysql ya está corriendo."
+
+fi
