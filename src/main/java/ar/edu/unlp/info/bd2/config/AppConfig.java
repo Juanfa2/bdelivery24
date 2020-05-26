85ca32e39a079d699b0072893930bba99eca677b
Luiggi <franceschi.luis@inta.gob.ar>
2020-05-26 18:53:45 -0300

    agregando archivos del TP3

--- src/main/java/ar/edu/unlp/info/bd2/config/AppConfig.java
+++ src/main/java/ar/edu/unlp/info/bd2/config/AppConfig.java
@@ -1,5 +1,6 @@
 package ar.edu.unlp.info.bd2.config;
 
+import ar.edu.unlp.info.bd2.repositories.DBliveryMongoRepository;
 import ar.edu.unlp.info.bd2.repositories.DBliveryRepository;
 import ar.edu.unlp.info.bd2.services.*;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +10,12 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public DBliveryService createService() {
-        DBliveryRepository repository = this.createRepository();
+        DBliveryMongoRepository repository = this.createRepository();
         return new DBliveryServiceImpl(repository);
     }
 
     @Bean
-    public DBliveryRepository createRepository() {
-        return new DBliveryRepository();
+    public DBliveryMongoRepository createRepository() {
+        return new DBliveryMongoRepository();
     }
 }
